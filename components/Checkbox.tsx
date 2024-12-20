76ca2bc53740c28ea7b18b2d946f85d28509c4de
Meetpatel006 <136876547+Meetpatel006@users.noreply.github.com>
2024-12-21 00:21:18 +0530

    Update Checkbox.tsx

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -1,4 +1,4 @@
-const Checkbox = ({handleChange, name, checked}) => {
+const Checkbox = ({handleChange, name, checked}: {handleChange: any, name: any, checked: any}) => {
     return (
       <input type="checkbox" checked={checked} onChange={handleChange} name={name} id={name} className="setting-checkbox"></input>
     )
