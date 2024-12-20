370a417e54ce66dfd6ff5fd9bd6e3156f41cea81
Meetpatel006 <136876547+Meetpatel006@users.noreply.github.com>
2024-12-21 00:22:00 +0530

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,10 @@
-const Input = ({type, handleChange, name}) => {
+interface InputProps {
+  type: string;
+  handleChange: React.ChangeEventHandler<HTMLInputElement>;
+  name: string;
+}
+
+const Input: React.FC<InputProps> = ({type, handleChange, name}) => {
   return (
     <input type={type} onInput={handleChange} name={name} id={name} className="setting-input"></input>
   )
